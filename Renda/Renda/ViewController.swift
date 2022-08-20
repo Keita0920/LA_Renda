9510a5a8fa42f3486ec45308df7aa7ca8c7ce73c
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-20 16:35:24 +0900

    fainal

--- Renda/Renda/ViewController.swift
+++ Renda/Renda/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    @IBOutlet var tapButton: UIButton!
+    
+    var tapCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tapButton.layer.cornerRadius = 125
+    }
+    
+    
+    
+    @IBAction func tapTapButton() {
+        
+        tapCount = tapCount + 1
+        
+        countLabel.text = String(tapCount)
     }
 
 
