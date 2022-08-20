d0420803ad2ac1096ff161d81d9be95ef5652537
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-20 15:07:49 +0900

    rendatest

---
+++ Renda/Renda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by K I on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
